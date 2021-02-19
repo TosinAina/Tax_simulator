8c7e9f504ba0efb997d4a7a673af1035c7b9e35b
Olajide Oluwtosin <oluwatosinaina424@gmail.com>
2021-02-19 22:30:21 +0100

    updates

--- app.py
+++ app.py
@@ -2,10 +2,12 @@ import pickle
 import streamlit as st
 import numpy as np
 
-classifier = pickle.load(open('classifier.pkl','rb'))
-scaler = pickle.load(open('scaler.pkl','rb'))
+grosspay_regressor = pickle.load(open('grosspay_regressor.pkl','rb'))
+grosspay_scaler = pickle.load(open('grosspay_scaler.pkl','rb'))
+netpay_regressor = pickle.load(open('netpay_regressor.pkl','rb'))
+netpay_scaler = pickle.load(open('netpay_scaler.pkl','rb'))
 
-def prediction(a):
+def netpay_prediction(a):
     Pension = 0.08 * a
     NHF = 0.025 * a
     Standard_relief = 0.2 * a
@@ -13,25 +15,51 @@ def prediction(a):
     Total_relief = Pension + NHF + Personal_relief + Standard_relief
     Net_taxable = a - Total_relief
     a = np.array([a,Pension,NHF,Standard_relief,Personal_relief,Total_relief,Net_taxable])
-    a = scaler.transform(a.reshape(1,-1))
+    a = netpay_scaler.transform(a.reshape(1,-1))
+    netpay = netpay_regressor.predict(a)
+    return netpay
 
-    prediction = classifier.predict(a)
-    return prediction
+
+def grosspay_prediction(a):
+    a = np.array(a)
+    a = grosspay_scaler.transform(a.reshape(1,-1))
+    grosspay = grosspay_regressor.predict(a)
+    return grosspay
 
 def app():
-    html_temp = """
-    <div style ="background-color:blue;padding:13px"> 
-    <h1 style ="color:black;text-align:center;">Streamlit Net pay ML App</h1> 
-    </div> 
-    """
 
-    st.markdown(html_temp,unsafe_allow_html = True)
+    menu = ['Predict Netpay','Predict Grosspay']
+    options = st.sidebar.selectbox("Menu",menu)
 
-    gross_pay = st.number_input('What is your gross pay')
+    if options == 'Predict Netpay':
+        html_temp = """
+        <div style ="background-color:blue;padding:13px"> 
+        <h1 style ="color:black;text-align:center;">Streamlit Net pay ML App</h1> 
+        </div> 
+        """
 
-    if st.button('Predict'):
-        net_pay = prediction(gross_pay)
-        st.success(f'Your net pay will be {net_pay}')
+        st.markdown(html_temp,unsafe_allow_html = True)
+
+        gross_pay = st.number_input('What is your gross pay')
+
+        if st.button('Predict'):
+            net_pay = netpay_prediction(gross_pay)
+            st.success(f'Your net pay will be {net_pay}')
+
+    else:
+        html_temp = """
+        <div style ="background-color:blue;padding:13px"> 
+        <h1 style ="color:black;text-align:center;">Streamlit Gross pay ML App</h1> 
+        </div> 
+        """
+
+        st.markdown(html_temp,unsafe_allow_html = True)
+
+        net_pay = st.number_input('What is your net pay')
+
+        if st.button('Predict'):
+            gross_pay = grosspay_prediction(net_pay)
+            st.success(f'Your gross pay will be {gross_pay}')
 
 if __name__ == '__main__':
     app()
